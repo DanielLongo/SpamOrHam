b80fde06154eff2e04013c578fed203745e54d06
Daniel Longo <DanielLongo@daniels-mbp-3.lan>
2018-06-27 20:04:15 -0700

    updates

--- read_messages.py
+++ read_messages.py
@@ -3,13 +3,42 @@ import sklearn
 from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 from sklearn.naive_bayes import MultinomialNB
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
 
+def porter_stemmer(text, ps):
+	final = []
+	text = text.split(" ")
+	for word in text:
+		try:
+			word = word_tokenize(word)
+			if len(word) == 0:
+				continue
+			word = word[0] #remvoes extra punctuation
+			word = ps.stem(word)
+			print(word)
+			final += [word]
+		except Exception as e:
+			print("ERROROROR", e)
+			print(word)
+			raise Exception("LOOOK")
+	return final
+
+
+	# return [ps.stem(word_tokenize(word)[-1]) for word in text]
 
 def read_messages(filepath):
 	data = pd.read_csv("./spam.csv", encoding="latin1", names=["labels", "text","","",""])
 	data = data.filter(["labels", "text"]) #remove extra columns
 	mapping = {"spam" : 0, "ham" : 1}
-	data = data.replace({"labels":mapping})		
+	data = data.replace({"labels":mapping})
+
+	ps = PorterStemmer()
+
+	for index, value in data.iterrows():
+		text = value["text"]
+		text = porter_stemmer(text, ps)
+		data.set_value(index, "text", text)
 
 	#counts the number of uses per word 
 	count_vectorizer = CountVectorizer()
